9deae64e176402d9e46a1b4c883906e0305ed04b
Tarlispace69 <116681079+Tarlispace69@users.noreply.github.com>
2023-10-25 13:25:17 +0300

    For-8-Ron complete

---
+++ For-8-Ron/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace For_8_Ron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0, i = 0;
+            long factorial = 1;
+
+            // receiving input for factorial end
+            Console.Write("Enter a number: ");
+            input = int.Parse(Console.ReadLine());
+
+            // calculating factorial sequentially
+            for (i=1; i <= input; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"The factorial for {input} (!{input}) is {factorial}");
+            Console.ReadLine();
+        }
+    }
+}
